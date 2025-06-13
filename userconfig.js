4f253c5e7f20a2f4f1f327585e35aa3f763b3855
vzneyv <132143070+vzneyv@users.noreply.github.com>
2025-06-13 17:23:51 +0100

    Update userconfig.js

--- userconfig.js
+++ userconfig.js
@@ -14,13 +14,13 @@ const default_configuration = {
     scale: "C",
   },
   clock: {
-    format: "k:i p",
+    format: "h:i",
     icon_color: palette.maroon,
   },
   additionalClocks: [
     {
-      label: "UA",
-      timezone: "Europe/London",
+      label: "IT",
+      timezone: "Europe/Vicenza",
       format: "h:i",
       icon_color: palette.peach,
     },
