4c69942bb34e2562f98d79f05adf267e9c3cd015
Navtan Kaulsi <132143070+vzneyv@users.noreply.github.com>
2025-09-09 22:46:13 +0100

    Update userconfig.js

--- userconfig.js
+++ userconfig.js
@@ -32,7 +32,7 @@ const default_configuration = {
   disabled: [],
   localIcons: true,
   localFonts: true,
-  fastlink: "https://www.perplexity.ai",
+  fastlink: "https://fmhy.net",
   openLastVisitedTab: true,
   tabs: [
     {
